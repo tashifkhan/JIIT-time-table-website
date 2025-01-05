2623dcb9d95176e467c3953de6eef0155c7aeb9d
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-01-05 05:42:36 +0530

    url

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")!).render(
 			<Router>
 				<div className="fixed right-4 bottom-4 z-50 p-5 rounded-[1.4rem] backdrop-blur-md bg-white/5 border border-white/30 shadow-lg hover:bg-white/30 transition-all duration-300">
 					<a
-						href="https://github.com/tashifkhan"
+						href="https://github.com/tashifkhan/JIIT-time-table-website"
 						target="_blank"
 						rel="noopener noreferrer"
 						className="text-[#F0BB78] hover:text-[#543A14] transition-colors"

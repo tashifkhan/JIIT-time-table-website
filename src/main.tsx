3cad4ff5abf64511300b353164216a2b4baa5715
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-01-05 07:24:01 +0530

    add @vercel/analytics package and integrate into main application

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,12 @@ import TimelinePage from "./components/timeline.tsx";
 import { Github } from "lucide-react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserContextProvider from "./context/userContextProvider.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<UserContextProvider>
+			<Analytics />
 			<Router>
 				<div className="fixed right-4 bottom-4 z-50 p-5 rounded-[1.4rem] backdrop-blur-md bg-white/5 border border-white/30 shadow-lg hover:bg-white/30 transition-all duration-300">
 					<a

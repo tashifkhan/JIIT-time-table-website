214aa7a458f4d0d4cbfc6ecc99b5b6b3c89dad05
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-01-06 21:20:29 +0530

    128 parser added

--- public/script.py
+++ public/script.py
@@ -1,5 +1,6 @@
 # Import and expose the time_table_creator function directly
 from modules.BE62_creator import time_table_creator
+from modules.BE128_creator import time_table_creator128
 
 # Ensure the function is available in global scope
 # globals()['time_table_creator'] = time_table_creator

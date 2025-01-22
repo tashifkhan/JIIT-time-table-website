9daeec8aea7abba0c0c7525e2148b791d2bce802
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-01-22 17:34:25 +0530

    1st year 128 handelled gracefully

--- public/script.py
+++ public/script.py
@@ -1,5 +1,5 @@
 # Import and expose the time_table_creator function directly
-from _creator import time_table_creator, banado128
+from _creator import time_table_creator, banado128, bando128_year1
 
 # Ensure the function is available in global scope
 # globals()['time_table_creator'] = time_table_creator

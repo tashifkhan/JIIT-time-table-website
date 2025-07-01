c3e213e77a1310dc9b059a53752b9834acde5ffa
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-07-01 16:39:30 +0530

    umm don't like it much this is the best i could do lets see

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -12,7 +12,9 @@ const TabsList = React.forwardRef<
 	<TabsPrimitive.List
 		ref={ref}
 		className={cn(
-			"inline-flex h-12 items-center justify-center rounded-2xl bg-white/10 backdrop-blur-md border border-[#F0BB78]/30 shadow-lg p-1 text-[#F0BB78]",
+			"inline-flex items-center justify-center rounded-2xl bg-white/10 backdrop-blur-md border border-[#F0BB78]/30 shadow-lg p-1 text-[#F0BB78]",
+			"data-[orientation=vertical]:flex-col data-[orientation=vertical]:h-auto data-[orientation=vertical]:w-full data-[orientation=vertical]:rounded-xl",
+			"data-[orientation=horizontal]:h-12 data-[orientation=horizontal]:w-full",
 			className
 		)}
 		{...props}
@@ -28,6 +30,8 @@ const TabsTrigger = React.forwardRef<
 		ref={ref}
 		className={cn(
 			"inline-flex items-center justify-center whitespace-nowrap rounded-xl px-5 py-2 text-base font-semibold transition-all duration-200 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#F0BB78] focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-[#F0BB78]/80 hover:text-[#F0BB78] data-[state=active]:bg-[#F0BB78]/20 data-[state=active]:text-[#F0BB78] data-[state=active]:shadow-md data-[state=active]:border-b-2 data-[state=active]:border-[#F0BB78]",
+			"data-[orientation=vertical]:w-full data-[orientation=vertical]:justify-start data-[orientation=vertical]:data-[state=active]:border-b-0 data-[orientation=vertical]:data-[state=active]:border-l-2 data-[orientation=vertical]:data-[state=active]:border-b-0 data-[orientation=vertical]:rounded-lg",
+			"data-[orientation=horizontal]:data-[state=active]:border-b-2 data-[orientation=horizontal]:data-[state=active]:border-l-0",
 			className
 		)}
 		{...props}
@@ -42,7 +46,7 @@ const TabsContent = React.forwardRef<
 	<TabsPrimitive.Content
 		ref={ref}
 		className={cn(
-			"mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+			"mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#F0BB78] focus-visible:ring-offset-2",
 			className
 		)}
 		{...props}

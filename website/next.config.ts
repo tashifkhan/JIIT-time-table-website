ee26337d3af93c6ac25f37a4d19ffe8168967a97
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-12-04 23:02:17 +0530

    fixed posthog shit

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/ph/:path*",
+        destination: "https://eu.posthog.com/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;

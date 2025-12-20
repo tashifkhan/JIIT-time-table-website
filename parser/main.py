56ef30e799d744e24015a747ae1ea03c169f2cd2
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-12-20 20:33:47 +0530

    imported modules

---
+++ parser/main.py
@@ -0,0 +1,7 @@
+from BCA.creator import creator as creator_bca
+from sector_62.creator import creator as creator_btech
+from sector_128.creator import creator as creator_mca
+
+
+if __name__ == "__main__":
+    ...
